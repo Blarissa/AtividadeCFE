989b9ae206efd6f2b4768551cf18dfa5c00681cb
Larissa Brasil <54214827+Blarissa@users.noreply.github.com>
2024-02-14 17:42:53 -0300

    feat: adicionando validação de login

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ function topo(){
     window.scrollTo(0, 0, 'smooth');
 }
 //Validação de Login
+function login(){
+    var logado = 0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario = usuario.toLowerCase();
+    
+    var senha = document.getElementById('senha').value;
+    senha = senha.toLowerCase();
+    
+    if(usuario == "admin" && senha == "123456"){
+        window.location = "index.html";
+        logado = 1;
+    }
+    if(logado == 0){
+        alert("Acesso Negado. Dados incorretos");
+    }    
+}
 //Ativar alert no botão cadastrar
